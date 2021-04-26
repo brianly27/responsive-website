0dc1e1102adf350c1592b20a784a86693243d936
brianly27 <brian.kyang.ly@gmail.com>
2021-04-26 10:21:04 -0700

    added responsive header

---
+++ app.js
@@ -0,0 +1,14 @@
+function toggleClassName() {
+    let sidebar = document.querySelector('.sidebar');
+    let wrapper = document.querySelector('.wrapper');
+    let toggle = document.querySelector('.toggle');
+
+    sidebar.classList.toggle('active');
+    wrapper.classList.toggle('active');
+    toggle.classList.toggle('active');
+}
+
+window.addEventListener('scroll', function(){
+    const header = document.querySelectorAll('header'); //error, querySelector is not finding the header element
+    header.classlist.toggle("sticky", window.scrollY > 0 );
+})
