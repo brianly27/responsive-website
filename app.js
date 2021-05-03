9b36bd10d47e9bbdf653d98bde93f914ccc91466
brianly27 <brian.kyang.ly@gmail.com>
2021-05-02 23:35:09 -0700

    minor edits

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-sal({
-    threshold: 1,
-    once: false,
-  });
-  
-function toggleClassName() {
-    let sidebar = document.querySelector('.sidebar');
-    let wrapper = document.querySelector('.wrapper');
-    let toggle = document.querySelector('.toggle');
+// sal({
+//     threshold: 1,
+//     once: false,
+//   });
 
-    sidebar.classList.toggle('active');
-    wrapper.classList.toggle('active');
-    toggle.classList.toggle('active');
-}
+// function toggleClassName() {
+//     let sidebar = document.querySelector('.sidebar');
+//     let wrapper = document.querySelector('.wrapper');
+//     let toggle = document.querySelector('.toggle');
 
-window.addEventListener('scroll', function(){
-    const header = document.querySelectorAll('header'); //error, querySelector is not finding the header element
-    header.classlist.toggle("sticky", window.scrollY > 0 );
-})
+//     sidebar.classList.toggle('active');
+//     wrapper.classList.toggle('active');
+//     toggle.classList.toggle('active');
+// }
+
+// window.addEventListener('scroll', function(){
+//     const header = document.querySelectorAll('header'); //error, querySelector is not finding the header element
+//     header.classlist.toggle("sticky", window.scrollY > 0 );
+// })
